7baa9fa5e6f60420ebd1acac175b271a5b98a9d3
konstantinkl <k.klassen@21torr.com>
2019-05-15 13:17:20 +0200

    MMM live coding project

---
+++ app/src/main/java/com/torr/coroutinesplayground/Example_03_Delay_Suspend_Global_Default.kt
@@ -0,0 +1,47 @@
+package com.torr.coroutinesplayground
+
+import android.util.Log
+import kotlinx.android.synthetic.main.fragment_example.*
+import kotlinx.android.synthetic.main.fragment_example.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class Example_03_Delay_Suspend_Global_Default : BaseFragment() {
+
+    override fun startRequests() {
+        GlobalScope.launch {
+            showLoading(loginRequestView)
+            val user = login()
+            showSuccess(loginRequestView)
+
+            showLoading(friendsRequestView)
+            val friends = loadFriends(user)
+            showSuccess(friendsRequestView)
+
+            showLoading(postsRequestView)
+            val posts = loadPosts(user)
+            showSuccess(postsRequestView)
+
+            doSomething(friends, posts)
+        }
+    }
+
+    suspend fun login(): User {
+        delay(2000)
+        Log.d("MMM", "User authenticated on thread ${Thread.currentThread()}")
+        return User()
+    }
+
+    suspend fun loadFriends(user: User): List<User> {
+        delay(2000)
+        Log.d("MMM", "Friends loaded on thread ${Thread.currentThread()}")
+        return listOf()
+    }
+
+    suspend fun loadPosts(user: User): List<Any> {
+        delay(2000)
+        Log.d("MMM", "Posts loaded on thread ${Thread.currentThread()}")
+        return listOf()
+    }
+}
---
+++ app/src/main/java/com/torr/coroutinesplayground/Example_13_AsyncTask.kt
@@ -0,0 +1,72 @@
+package com.torr.coroutinesplayground
+
+import android.annotation.SuppressLint
+import android.os.AsyncTask
+import android.util.Log
+import kotlinx.android.synthetic.main.fragment_example.*
+
+@SuppressLint("StaticFieldLeak")
+class Example_13_AsyncTask : BaseFragment() {
+
+    override fun startRequests() {
+        object : RequestTask<User>(loginRequestView) {
+            override fun doInBackground(vararg params: Unit?) = login()
+
+            override fun onPostExecute(user: User) {
+                if (activity != null && !activity?.isFinishing!!) {
+                    super.onPostExecute(user)
+
+                    object : RequestTask<List<User>>(friendsRequestView) {
+                        override fun doInBackground(vararg params: Unit?) = loadFriends(user)
+
+                        override fun onPostExecute(friends: List<User>) {
+                            if (activity != null && !activity?.isFinishing!!) {
+                                super.onPostExecute(friends)
+
+                                object : RequestTask<List<Any>>(postsRequestView) {
+                                    override fun doInBackground(vararg params: Unit?) = loadPosts(user)
+
+                                    override fun onPostExecute(posts: List<Any>) {
+                                        if (activity != null && !activity?.isFinishing!!) {
+                                            super.onPostExecute(posts)
+
+                                            doSomething(friends, posts)
+                                        }
+                                    }
+                                }.execute()
+                            }
+                        }
+                    }.execute()
+                }
+            }
+        }.execute()
+    }
+
+    fun login(): User {
+        Thread.sleep(2000)
+        Log.d("MMM", "User authenticated on thread ${Thread.currentThread()}")
+        return User()
+    }
+
+    fun loadFriends(user: User): List<User> {
+        Thread.sleep(2000)
+        Log.d("MMM", "Friends loaded on thread ${Thread.currentThread()}")
+        return listOf()
+    }
+
+    fun loadPosts(user: User): List<Any> {
+        Thread.sleep(2000)
+        Log.d("MMM", "Posts loaded on thread ${Thread.currentThread()}")
+        return listOf()
+    }
+
+    abstract inner class RequestTask<T>(val loadSuccessTextView: LoadSuccessTextView) : AsyncTask<Unit, Unit, T>() {
+        override fun onPreExecute() {
+            showLoading(loadSuccessTextView)
+        }
+
+        override fun onPostExecute(result: T) {
+            showSuccess(loadSuccessTextView)
+        }
+    }
+}
